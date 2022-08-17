c7d4494f3d7ca9b95e800819e306a3564f53080f
Domnewmarch <dom.newmarch@parall.ax>
2022-08-17 14:09:13 +0100

    update footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="mt-12 flex flex-col items-center justify-center">
-      <a rel="norefferer" href="https://www.envydigital-webdesign.co.uk">
+      <a rel="norefferer" href="https://www.parall.ax">
         <img className=" w-3/5 items-center m-auto" src="./img/parallax-logo-dark.png" alt="" />
       </a>
     </div>
