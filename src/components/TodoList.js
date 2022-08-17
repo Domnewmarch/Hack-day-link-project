0f1c9e2198f9e57de777262d59b025dfa59f950e
Domnewmarch <dom.newmarch@parall.ax>
2022-08-17 11:20:01 +0100

    tidy up repo

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import Todo from './Todo'
+
+export default function TodoList({ todoList, user }) {
+  return (
+    <section>
+      <ul>
+        {/*if there are todos in the list...*/}
+        {todoList.length >= 1
+          ? todoList.map((todo, idx) => {
+              //map only the user's todos
+              return user.email == todo.userEmail ? <Todo key={todo._id} todo={todo} /> : ''
+            })
+          : 'Enter a todo item'}
+      </ul>
+    </section>
+  )
+}
