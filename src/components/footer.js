6096314c7f1ef696d9430267580c481c3da68967
Domnewmarch <dom.newmarch@parall.ax>
2022-08-17 14:20:33 +0100

    add return to dashboard

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
       <a rel="norefferer" href="https://www.parall.ax">
         <img className=" w-3/5 items-center m-auto" src="./img/parallax-logo-dark.png" alt="" />
       </a>
+      <a href="https://hack-day-link-project.vercel.app/dashboard">Edit your links</a>
     </div>
   )
 }
